f749960ac16d3102b2800d16112b3c7be0bd8bb7
Samsegun <oyebadesamsegun@gmail.com>
2023-07-07 22:15:21 +0100

    packages

---
+++ src/dev/lpa/Main.java
@@ -0,0 +1,14 @@
+package dev.lpa;
+
+
+import com.abc.first.Item;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Item item = new Item("book");
+//        com.abc.first.Item item = new com.abc.first.Item("book");
+
+        System.out.println(item);
+    }
+}
